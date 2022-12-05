0855aa589e977d41acc044cb364b39b6f4b1026a
x6di <x6deincognita@gmail.com>
2022-12-05 19:04:11 +0100

    library created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name='llimcobe',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Lossless Image Compression Benchmark',
+    author='Xavier Fernàndez Mellado',
+    license='MIT',
+    install_requires=[],
+    setup_reqires=[],
+)
---
+++ src/llimcobe.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
+
+
+class Llimcobe(ABC):
+    def __init__(self):
+        super().__init__()
+        self.models = {}
+        self.dataset = self.prepare_dataset()
+
+    @abstractmethod
+    def prepare_dataset(self):
+        """
+        This abstract method will be used to prepare dataset.
+        :return: A list of images in ____ format.
+        """
+        pass
+
+    def preprocess_dataset(self):
+        pass
+
+    def set_model(self, name, model):
+        """
+        This function includes into a dictionary the model.
+        If the name exists in models dictionary the model will be overwritten.
+
+        :param name: (str) Name of the model to be allocated.
+        :param model: Model.
+        :return: True if model is included, false if not
+        """
+        if name and model:
+            self.models[name] = model
+            return True
+
+        return False
+
+    def get_model(self, name):
+        """
+        This function will return the model if exists or False if not exists.
+        :param name: The name of the model
+        :return: The model if exists, False if not.
+        """
+        if name in self.models.keys():
+            return self.models[name]
+        return False
+
+    def del_model(self, name):
+        """
+        This function will delete the model if exists.
+        :param name: The name of the model
+        :return: True if the model is deleted or false if not.
+        """
+        if name in self.models.keys():
+            del self.models[name]
+            return True
+        return False
+
+    def benchmark(self):
+        for image in self.dataset:
+            pass
+
